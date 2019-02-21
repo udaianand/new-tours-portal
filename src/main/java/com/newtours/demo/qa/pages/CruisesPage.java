55e4154cc07718d6e77669f3b8943e2e9a871552
Udai Anand <udaianand.m@gmail.com>
2019-02-21 14:44:13 +0530

    added Cruise page

---
+++ src/main/java/com/newtours/demo/qa/pages/CruisesPage.java
@@ -0,0 +1,10 @@
+package com.newtours.demo.qa.pages;
+
+import com.newtours.demo.qa.base.TestBase;
+
+public class CruisesPage extends TestBase {
+
+	void searchCruise() {
+		System.out.println("Search for the desired Cruise...");
+	}
+}
